bf5b3d07af3627d0ef09c3039c3c443274498fcc
vijay97bk <75747707+vijay97bk@users.noreply.github.com>
2021-03-10 18:36:39 +0530

    [vijay]AddedContacts

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -3,7 +3,7 @@ select * from sys.databases;
 use AddressBook;
 
 create table Address_Book(
-FIrstName varchar(100) not null,
+FirstName varchar(100) not null,
 LastName varchar(100) not null,
 Address varchar(150),
 City Varchar(50),
@@ -11,3 +11,12 @@ PhoneNumber bigint,
 Email_ID varchar(50));
 
 select * from Address_Book;
+
+insert into Address_Book(FirstName,LastName,City,PhoneNumber,Email_ID)
+values ('Ramesh', 'Babu', 'Chennai', 9876543210, 'abc.gmail.com');
+
+insert into Address_Book(FirstName,LastName,City,PhoneNumber,Email_ID)
+values ('Dilip', 'Rathod', 'Mysore', 9765432180, 'def.gmail.com'),
+('Sarvesh', 'Mathpati', 'Bidar', 9638527410, 'ghi.gmail.com'),
+('Sonal', 'Karle', 'Mumbai', 7896541230, 'jkl.gmail.com');
+
